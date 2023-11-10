e6e987d8b464f8ebdfa027a9a0350c3f43b923ac
Uone14 <yuandika73@gmail.com>
2023-11-11 00:31:18 +0700

    tambah soal1

---
+++ soal1.py
@@ -0,0 +1,34 @@
+j = int(input('masukkan jumlah perulangan: '))
+for b in range(j):
+    a = input('masukkan kata : ')
+    for i in range (len(a)-1):
+        if (ord(a[i])-ord(a[i+1])) < 0:
+            print(a[i], 'kurang dari', a[i+1], 'selisihnya ialah', -(ord(a[i])-ord(a[i+1])))
+        elif (ord(a[i])-ord(a[i+1])) > 0:
+            print(a[i], 'lebih dari',a[i+1], 'selisihnya ialah', (ord(a[i])-ord(a[i+1])))
+        else:
+            print(a[i], 'sama dengan', a[i+1], 'selisihnya ialah', (ord(a[i])-ord(a[i+1])))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+    
+
+
+
+
+
